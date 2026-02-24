95b6580261a459d2ce24d806860523f3944203b7
tiloschankarki <tiloschankarki@gmail.com>
2026-02-23 22:23:09 -0600

    Added custom user model and initial migrations

--- config/settings.py
+++ config/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    "apps.accounts.apps.AccountsConfig",
+    "apps.dashboard.apps.DashboardConfig",
+    "apps.transactions.apps.TransactionsConfig",
+    "apps.goals.apps.GoalsConfig",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
