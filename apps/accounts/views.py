08ecff73bd76ee17da8311963b5f977b5823fa6a
Tiloschan <jov20@txstate.edu>
2026-03-25 20:50:54 -0500

    Login feature addition with password visibility.

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
-# Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("/")  # later we’ll point this to dashboard
+        else:
+            messages.error(request, "Invalid username or password")
+
+    return render(request, "accounts/login.html")
