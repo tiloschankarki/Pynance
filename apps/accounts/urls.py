0af947ae19422baaab82703ab495fd200de02a9e
Tiloschan <jov20@txstate.edu>
2026-03-25 20:59:24 -0500

    Added registration logic

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -5,4 +5,6 @@ app_name = "accounts"
 
 urlpatterns = [
     path("login/", views.login_view, name="login"),
+    path("register/", views.register_view, name="register"),
+
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import RegisterForm
+
 
 def login_view(request):
     if request.method == "POST":
@@ -11,8 +13,22 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
-            return redirect("/")  # later we’ll point this to dashboard
+            return redirect("/")
         else:
             messages.error(request, "Invalid username or password")
 
     return render(request, "accounts/login.html")
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Account created successfully.")
+            return redirect("/")
+    else:
+        form = RegisterForm()
+
+    return render(request, "accounts/register.html", {"form": form})
