4175aba5a7e193a25782b56d374aa7dd349798b0
Tiloschan <jov20@txstate.edu>
2026-03-25 21:48:14 -0500

    Added logout logic and improved login
    made it so that logged in users cannot acces login or register again

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -5,6 +5,9 @@ from .forms import RegisterForm
 
 
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("/")
+
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
@@ -21,6 +24,9 @@ def login_view(request):
 
 
 def register_view(request):
+    if request.user.is_authenticated:
+        return redirect("/")
+
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
@@ -32,3 +38,8 @@ def register_view(request):
         form = RegisterForm()
 
     return render(request, "accounts/register.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("accounts:login")
+
