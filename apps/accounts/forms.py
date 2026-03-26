23508546fbfa2fd9e890acbc5b45ad1902218321
Tiloschan <jov20@txstate.edu>
2026-03-25 21:48:59 -0500

    added register template and made changes to the forms

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
+User = get_user_model()
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -9,3 +10,13 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+        
+        # Add IDs for JS targeting
+        self.fields["password1"].widget.attrs["id"] = "password1"
+        self.fields["password2"].widget.attrs["id"] = "password2"
