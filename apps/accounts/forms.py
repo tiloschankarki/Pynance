e15e1133e790d37706355ff243e2d3b3070a11bd
Tiloschan <jov20@txstate.edu>
2026-04-20 12:00:08 -0500

    Added settings feature within the navbar

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 User = get_user_model()
 
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
@@ -16,7 +17,20 @@ class RegisterForm(UserCreationForm):
 
         for field_name, field in self.fields.items():
             field.widget.attrs["class"] = "form-control"
-        
-        # Add IDs for JS targeting
+
         self.fields["password1"].widget.attrs["id"] = "password1"
         self.fields["password2"].widget.attrs["id"] = "password2"
+
+
+class UsernameUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["username"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["username"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Enter new username",
+        })
--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("register/", views.register_view, name="register"),
     path('logout/', LogoutView.as_view(next_page='accounts:login'), name='logout'),
+    path("settings/", views.settings_view, name="settings"),
 
 ]
